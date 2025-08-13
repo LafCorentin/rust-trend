930b5be2f8915fa13c017d731453e94b9d6b70eb
Tuareg <corentin.lafond@gmail.com>
2025-08-13 21:50:18 +0200

    simplify sanitizer

--- src/utils.rs
+++ src/utils.rs
@@ -1,7 +1,3 @@
 pub fn sanitize_response(body: &str, pos: usize) -> &str {
-    let mut chars = body.chars();
-    for _ in 0..pos {
-        chars.next();
-    }
-    chars.as_str()
+    &body[pos..]
 }
